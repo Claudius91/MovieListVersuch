28fb0621e983589c33b83b3f5d6d899c443bbd01
claudius <claude-1991@hotmail.com>
2017-12-19 12:51:22 +0100

    neuer änderung

--- app/src/main/java/com/example/julian/movielistbeta/MovieListDataSource.java
+++ app/src/main/java/com/example/julian/movielistbeta/MovieListDataSource.java
@@ -46,7 +46,7 @@ class MovieListDataSource {
     };
 
     /**
-     * Erstellt eine MovieListDataSource durch die, mithilfe eines {@link MovieListDbHelper}, die Datenbank verwaltet wird
+     * Erstellt einennn MovieListDataSource durch die, mithilfe eines {@link MovieListDbHelper}, die Datenbank verwaltet wird
      *
      * @param c Kontext in dem die Datenbank genutzt wird
      */
